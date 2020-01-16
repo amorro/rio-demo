1dbe9ee51cf9aa646834adee2af26d5fcb9daaaf
amorro <44436902+amorro@users.noreply.github.com>
2020-01-17 00:13:49 +0100

    Initial

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Fprintln(w, "Hi there, I am rio:v2")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
